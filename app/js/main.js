f7846ecc334f5954255dfd38973e6c425c6c6427
Garmanika <aleks.orlov97@gmail.com>
2021-07-26 13:00:00 +0300

    26/07 diner

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function() {
-
+    $(".menu-btn").on("click", function() {
+        $(".header-bottom").fadeToggle();
+        $(".menu-btn").toggleClass("open-menu");
+        $(".header-wrapper").toggleClass("open-menu");
+        $("body").toggleClass("noscroll");
+    });
     $(".masked-phone").inputmask({
         mask: "+7 (999) 999-99-99",
         clearIncomplete: true,
