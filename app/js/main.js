805cd36221c93fbe1a601619841d496f2bf2987b
Garmanika <aleks.orlov97@gmail.com>
2021-08-20 16:44:05 +0300

    20/08

--- app/js/main.js
+++ app/js/main.js
@@ -1,72 +1,55 @@
-$(function() {
-	function mobileSearchTrigger() {
+$(function () {
+    function mobileSearchTrigger() {
 
-    $('.mobile-search-trigger').on('click', function(e) {
-        e.preventDefault();
-        $('.header-search-form').fadeIn();
-    });
-    $('.mobile-search-close').on('click', function(e) {
-        e.preventDefault();
-        $('.header-search-form').fadeOut();
-    });
+        $('.mobile-search-trigger').on('click', function (e) {
+            e.preventDefault();
+            $('.header-search-form').fadeIn();
+        });
+        $('.mobile-search-close').on('click', function (e) {
+            e.preventDefault();
+            $('.header-search-form').fadeOut();
+        });
 
-}
-
-
-
-function menuToggle() {
-
-    $('.mobile-navigation-trigger').on('click', function() {
-        if ($('.header-icons-search').hasClass('open-search') && !$('.mobile-navigation-trigger ').hasClass('active')) {
-            $('.panel-search').slideUp();
-            $('.header-icons-search').removeClass('open-search');
-        }
-        $(this).toggleClass('active');
-        $('body').toggleClass('hide-overflow-mobile');
-        $('ul.header-desktop-menu').slideToggle();
-        $('.header-desktop-menu').toggleClass('active');
-        $('.header-sub-menu-position').removeClass('mobile-navigation-sub-position');
-    });
-
-    $('.header-desktop-menu .is-parent > a').on('click', function(e) {
-        e.preventDefault();
-        var subMenu = $(this).next('.mobile-navigation-sub-position');
-        subMenu.addClass('sub-opened');
-    });
-
-    $('.header-desktop-sub-menu-heading').on('click', function(e) {
-        e.preventDefault();
-        var subMenu = $(this).closest('.mobile-navigation-sub-position');
-        subMenu.removeClass('sub-opened');
-    });
-
-    $(document).on('click', function(event) {
-
-        if (!$(event.target).closest('.header-desktop-menu .is-parent').length) {
-            $('.mobile-navigation-sub-position').removeClass('sub-opened');
-        }
-
-    });
+    }
 
 
 
-}
+    function menuToggle() {
 
+        $('.mobile-navigation-trigger').on('click', function () {
+            if ($('.header-icons-search').hasClass('open-search') && !$('.mobile-navigation-trigger ').hasClass('active')) {
+                $('.panel-search').slideUp();
+                $('.header-icons-search').removeClass('open-search');
+            }
 
+            $(this).toggleClass('active');
+            $('body').toggleClass('hide-overflow-mobile');
+            $('ul.header-desktop-menu').slideToggle();
+            $('.header-desktop-menu').toggleClass('active');
+            $('.header-sub-menu-position').removeClass('mobile-navigation-sub-position');
+        });
 
-$(document).ready(function() {
+        $('.header-desktop-menu .is-parent > a').on('click', function (e) {
+            e.preventDefault();
+            var subMenu = $(this).next('.mobile-navigation-sub-position');
+            subMenu.addClass('sub-opened');
+        });
 
-    mobileSearchTrigger();
-   
-    menuToggle();
-   
+        $('.header-desktop-sub-menu-heading').on('click', function (e) {
+            e.preventDefault();
+            var subMenu = $(this).closest('.mobile-navigation-sub-position');
+            subMenu.removeClass('sub-opened');
+        });
 
-});
+        $(document).on('click', function (event) {
+            if (!$(event.target).closest('.header-desktop-menu .is-parent').length) {
+                $('.mobile-navigation-sub-position').removeClass('sub-opened');
+            }
 
+        });
+    }
 
-
-
-    $('.header-icons-search').on('click', function() {
+    $('.header-icons-search').on('click', function () {
         if (!$('.header-icons-search').hasClass('open-search') && $('.mobile-navigation-trigger').hasClass('active')) {
             $('ul.header-desktop-menu').slideUp();
             $('.mobile-navigation-trigger').removeClass('active');
@@ -102,5 +85,7 @@ $(document).ready(function() {
             }
         }
     })
+    mobileSearchTrigger();
 
+    menuToggle();
 });
