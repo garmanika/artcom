c783727a3af8367fcd8b6ff30ff9d03c70ba4610
Garmanika <aleks.orlov97@gmail.com>
2021-10-21 16:39:28 +0300

    21/10

--- app/js/main.js
+++ app/js/main.js
@@ -61,6 +61,15 @@ $(function () {
     resizeScreen();
     window.addEventListener("resize", resizeScreen);
 
+		$(".direction-catalog-content-aside-item-head").on("click", function (event) {
+			$(this)
+				.closest(".direction-catalog-content-aside-item")
+				.toggleClass("open");
+			// $(this)
+				// .closest(".direction-catalog-content-aside-item")
+				// .find(".direction-catalog-content-aside-item-content")
+		});
+
     $(".header-desktop-sub-menu-heading").on("click", function (e) {
       e.preventDefault();
       var subMenu = $(this).closest(".mobile-navigation-sub-position");
@@ -92,6 +101,50 @@ $(function () {
     clearIncomplete: true,
     showMaskOnHover: false,
   });
+	// Инициализация превью слайдера
+const sliderThumbs = new Swiper('.slider__thumbs .swiper', { // ищем слайдер превью по селектору
+	// задаем параметры
+	direction: 'vertical', // вертикальная прокрутка
+	slidesPerView: 3, // показывать по 3 превью
+	spaceBetween: 24, // расстояние между слайдами
+	navigation: { // задаем кнопки навигации
+		nextEl: '.slider__next', // кнопка Next
+		prevEl: '.slider__prev' // кнопка Prev
+	},
+	freeMode: true, // при перетаскивании превью ведет себя как при скролле
+	breakpoints: { // условия для разных размеров окна браузера
+		0: { // при 0px и выше
+			direction: 'horizontal', // горизонтальная прокрутка
+		},
+		768: { // при 768px и выше
+			direction: 'vertical', // вертикальная прокрутка
+		}
+	}
+});
+// Инициализация слайдера изображений
+const sliderImages = new Swiper('.slider__images .swiper', { // ищем слайдер превью по селектору
+	// задаем параметры
+	direction: 'vertical', // вертикальная прокрутка
+	slidesPerView: 1, // показывать по 1 изображению
+	spaceBetween: 32, // расстояние между слайдами
+	mousewheel: true, // можно прокручивать изображения колёсиком мыши
+	navigation: { // задаем кнопки навигации
+		nextEl: '.slider__next', // кнопка Next
+		prevEl: '.slider__prev' // кнопка Prev
+	},
+	grabCursor: true, // менять иконку курсора
+	thumbs: { // указываем на превью слайдер
+		swiper: sliderThumbs // указываем имя превью слайдера
+	},
+	breakpoints: { // условия для разных размеров окна браузера
+		0: { // при 0px и выше
+			direction: 'horizontal', // горизонтальная прокрутка
+		},
+		768: { // при 768px и выше
+			direction: 'vertical', // вертикальная прокрутка
+		}
+	}
+});
   const swiper4 = new Swiper(".project-slider", {
     slidesPerView: 3,
 
@@ -241,12 +294,5 @@ $(function () {
       slider.noUiSlider.setHandle(indexInput, input.value);
     });
   });
-  $(".direction-catalog-content-aside-item-head").on("click", function (event) {
-    $(this)
-      .closest(".direction-catalog-content-aside-item")
-      .toggleClass("open");
-    // $(this)
-      // .closest(".direction-catalog-content-aside-item")
-      // .find(".direction-catalog-content-aside-item-content")
-  });
+
 });
